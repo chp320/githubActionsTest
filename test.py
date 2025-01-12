16cb39a727c65505dedf5131e0adb311ff62c8c2
chp320 <chp320@naver.com>
2025-01-12 21:51:14 +0900

    log_entry 포맷 수정

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from datetime import datetime
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 # 추가할 내용 생성
-log_entry = f"실행 시각: {current_time}\n"
+log_entry = f'실행 시각: {current_time}\n'
 
 # README.md 파일에 실행 시각 추가
 with open('README.md', 'a', encoding='utf-8') as file:
